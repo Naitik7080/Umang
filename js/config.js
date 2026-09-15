a214e77854d10546fc8dc80d827d0730e2abde38
Naitik Shukla <naitikshukla9758@gmail.com>
2026-09-15 11:22:26 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,3 +0,0 @@
-// Set this to your deployed Railway backend URL once it's live, e.g.:
-// const API_BASE_URL = "https://umang-backend-production.up.railway.app";
-const API_BASE_URL = "http://localhost:8080";
